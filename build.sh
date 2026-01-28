67eb062be675b79e9c2bfae0a03dd74b0c89f694
Nil Geisweiller <ngeiswei@gmail.com>
2026-01-28 06:09:02 +0200

    Add standalone SWI-Prolog program to retrieve grandparents

---
+++ build.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+swipl -o gp -c gp.pl
---
+++ gp.pl
@@ -0,0 +1,15 @@
+% Standalone SWI-Prolog program that outputs all grandparents
+
+use_module(library(main)).
+:- initialization(main, main).
+
+% Parents groundings
+parent(jon, alex).
+parent(alex, louise).
+parent(alex, robert).
+
+% Grand parent rule
+grandparent(X, Y) :- parent(X, Z), parent(Z, Y).
+
+% Retrieve and print all grandparents
+main :- findall(gp(X, Y), grandparent(X, Y), R), writef("Results: %w\n", [R]).
